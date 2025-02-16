60695bbbb66bdfaa381a1330ffe6868125f2468b
Krishna <krishnamurugan589@gmail.com>
2025-02-17 00:24:08 +0530

    feat: Full fledged parser for 4-bit computer assembler

--- four-bit-computer/assembler.py
+++ four-bit-computer/assembler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from enum import Enum
-from typing import List, Callable
+from typing import List, Callable, Optional
 
 
 class TokenType(Enum):
@@ -56,7 +56,7 @@ class Scanner:
         while not self.is_at_end():
             self.start_position = self.current_position
             self.scan_token()
-        self.tokens.append(Token(TokenType.EOF, ""))
+        self.tokens.append(Token(TokenType.EOF.name, "EOF", line=self.line + 1))
 
     def scan_token(self):
         c: str = self.advance()
@@ -125,10 +125,6 @@ class Scanner:
     def isAlpha(self, c: str) -> bool:
         return (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z')
 
-    def match(self, expected_token: str) -> bool:
-        if (self.peek() == expected_token): return True
-        return False
-
     def advance(self) -> str:
         character = self.source[self.current_position]
         self.current_position += 1
@@ -168,30 +164,42 @@ class Parser:
 
     def parse_tokens(self):
         while not self.is_at_end():
-            self.parse_token()
+            instruction: Instruction = self.parse_token()
+            self.instructions.append(instruction)
 
-    def parse_token(self):
+    def parse_token(self) -> Instruction:
         current_token: Token = self.advance()
-        if current_token not in self.MNEMONICS:
-            raise SyntaxError(f"Unknown token {current_token}")
+        mnemonic: str = current_token.lexeme
+        if mnemonic not in self.MNEMONICS:
+            raise SyntaxError(f"Unknown mnemonic: {mnemonic} at line number: {current_token.line}")
+
+        # Right after the mnemonic we should have a number (data)
+        current_token: Token = self.advance()
+        operand: int = int(current_token.lexeme)
+
+        if operand not in [0, 1]:
+            raise SyntaxError(f"Unknown operand: {operand} at line number: {current_token.line}")
+
+        return Instruction(mnemonic, operand)
 
     ################ HELPER FUNCTIONS #############
-    def advance(self) -> Token:
-        self.current_position += 1
-        return self.tokens[self.current_position - 1]
+    def advance(self) -> Optional[Token]:
+        current_token: Token = self.peek()
+        if current_token:
+            self.current_position += 1
+            return current_token
+        raise Exception("Reached end of tokens")
 
-    def match(self, expected_token: Token) -> bool:
-        if self.peek() == expected_token:
-            self.advance()
-            return True
-        return False
-
-    def peek(self) -> str:
-        if self.is_at_end(): return "\0"
-        return self.tokens[self.current_position]
+    def peek(self) -> Optional[Token]:
+        if not self.is_at_end(): return self.tokens[self.current_position]
+        return None
 
     def is_at_end(self) -> bool:
         return (self.current_position >= len(self.tokens)) or (self.tokens[self.current_position].tt == TokenType.EOF.name)
+
+    @property
+    def get_instructions(self) -> List[Instruction]:
+        return self.instructions
     ################ END OF HELPER FUNCTIONS ######
 
 if __name__ == "__main__":
@@ -205,3 +213,4 @@ if __name__ == "__main__":
 
     parser = Parser(scanner.get_tokens)
     parser.parse_tokens()
+    print(parser.get_instructions)
