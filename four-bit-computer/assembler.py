56e398b4bfab752edde25e75998e1f2144c550f8
Krishna <krishnamurugan589@gmail.com>
2025-02-16 23:17:55 +0530

    chore: Added line meta data into the Scanner and implemented the Parser partially

--- four-bit-computer/assembler.py
+++ four-bit-computer/assembler.py
@@ -22,13 +22,14 @@ class TokenType(Enum):
 
 
 class Token:
-    def __init__(self, tt: TokenType, lexeme: str, literal: object = None):
+    def __init__(self, tt: TokenType, lexeme: str, literal: object = None, line: int = 0):
         self.tt = tt
         self.lexeme = lexeme
         self.literal = literal
+        self.line = line
 
     def __repr__(self):
-        return f"[Token type: {self.tt}, Lexeme: {self.lexeme}]"
+        return f"[Token type: {self.tt}, Lexeme: {self.lexeme}, Line: {self.line}]"
 
 
 class Scanner:
@@ -49,6 +50,7 @@ class Scanner:
         self.start_position = 0
         self.current_position = 0
         self.tokens: List[Token] = []
+        self.line: int = 1
 
     def scan_tokens(self):
         while not self.is_at_end():
@@ -91,7 +93,7 @@ class Scanner:
             self.advance()
         text: str = self.source[self.start_position:self.current_position]
         token: Token = Token(
-            TokenType.NUMBER.name, text
+            TokenType.NUMBER.name, text, line=self.line
         )
         self.add_token(token)
 
@@ -101,7 +103,7 @@ class Scanner:
         text: str = self.source[self.start_position:self.current_position]
         token_type: TokenType = self.keywords.get(text)
         token: Token = Token(
-            token_type, text
+            token_type, text, line=self.line
         )
         self.add_token(token)
 
@@ -128,8 +130,11 @@ class Scanner:
         return False
 
     def advance(self) -> str:
+        character = self.source[self.current_position]
         self.current_position += 1
-        return self.source[self.current_position - 1]
+        if character == "\n":
+            self.line += 1
+        return character
 
     def peek(self) -> str:
         if self.is_at_end(): return "\0"
@@ -144,6 +149,51 @@ class Scanner:
     ############### END OF HELPER FUNCTIONS ###########
 
 
+class Instruction:
+    def __init__(self, mnemonic: str, data: str):
+        self.mnemonic = mnemonic
+        self.data = data
+
+    def __repr__(self):
+        return f"Instruction({self.mnemonic}, {self.data})"
+
+class Parser:
+    
+    MNEMONICS = ["LOAD", "ADD", "SUB", "AND", "OR", "XOR", "JMP", "HLT"]
+
+    def __init__(self, tokens: List[Token]):
+        self.tokens = tokens
+        self.current_position = 0
+        self.instructions = []
+
+    def parse_tokens(self):
+        while not self.is_at_end():
+            self.parse_token()
+
+    def parse_token(self):
+        current_token: Token = self.advance()
+        if current_token not in self.MNEMONICS:
+            raise SyntaxError(f"Unknown token {current_token}")
+
+    ################ HELPER FUNCTIONS #############
+    def advance(self) -> Token:
+        self.current_position += 1
+        return self.tokens[self.current_position - 1]
+
+    def match(self, expected_token: Token) -> bool:
+        if self.peek() == expected_token:
+            self.advance()
+            return True
+        return False
+
+    def peek(self) -> str:
+        if self.is_at_end(): return "\0"
+        return self.tokens[self.current_position]
+
+    def is_at_end(self) -> bool:
+        return (self.current_position >= len(self.tokens)) or (self.tokens[self.current_position].tt == TokenType.EOF.name)
+    ################ END OF HELPER FUNCTIONS ######
+
 if __name__ == "__main__":
 
     with open("instructions.txt", "r") as f:
@@ -152,3 +202,6 @@ if __name__ == "__main__":
     scanner = Scanner(source)
     scanner.scan_tokens()
     print(scanner.get_tokens)
+
+    parser = Parser(scanner.get_tokens)
+    parser.parse_tokens()
