43b0f188aa101a548eaa6ca5d1b1d28d63bbf0fd
Krishna <krishnamurugan589@gmail.com>
2025-02-21 18:47:12 +0530

    chore: Added unit test for scanner

---
+++ four-bit-computer/assembler/test/test_scanner.py
@@ -0,0 +1,20 @@
+import unittest
+from scanner import Scanner
+from token_type import TokenType
+
+class TestScanner(unittest.TestCase):
+    
+    def test_mnemonics(self):
+        source = "LOAD 1"
+        scanner = Scanner(source)
+        scanner.scan_tokens()
+        tokens = scanner.get_tokens
+
+        self.assertEqual(tokens[0].tt, TokenType.LOAD.name)
+        self.assertEqual(tokens[0].lexeme, TokenType.LOAD.value)
+        self.assertEqual(tokens[1].tt, TokenType.NUMBER.name)
+        self.assertEqual(tokens[1].lexeme, str(1))
+
+
+if __name__ == "__main__":
+    unittest.main()
