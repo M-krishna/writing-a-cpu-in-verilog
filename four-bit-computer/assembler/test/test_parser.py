43fc3ef2f8642c14f6e253c84daf5b86e70f7d18
Krishna <krishnamurugan589@gmail.com>
2025-02-24 16:25:18 +0530

    chore: modified a test to test the github workflow

--- four-bit-computer/assembler/test/test_parser.py
+++ four-bit-computer/assembler/test/test_parser.py
@@ -16,7 +16,7 @@ class TestParser(unittest.TestCase):
         instructions: Instruction = parser.get_instructions
 
         self.assertEqual(instructions[0].mnemonic, TokenType.LOAD.name)
-        self.assertEqual(instructions[0].data, 1)
+        self.assertEqual(instructions[0].data, str(1))
 
 
 if __name__ == "__main__":
